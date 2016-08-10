baf10dbe664609df020a139c4c51c581594e4175
Eulerd <euler.volcano.dai@gmail.com>
2016-08-10 17:28:40 +0900

    はじめてのF*ck

--- Brain fuck/Form1.Designer.cs
+++ Brain fuck/Form1.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.InPutBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.OutPutBox = new System.Windows.Forms.TextBox();
+            this.Chars = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 129);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 160);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "F*ck";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InPutBox
+            // 
+            this.InPutBox.Location = new System.Drawing.Point(284, 149);
+            this.InPutBox.Multiline = true;
+            this.InPutBox.Name = "InPutBox";
+            this.InPutBox.Size = new System.Drawing.Size(243, 61);
+            this.InPutBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "BrainF*ck";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(282, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "InPut";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(282, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "OutPut";
+            // 
+            // OutPutBox
+            // 
+            this.OutPutBox.Location = new System.Drawing.Point(284, 228);
+            this.OutPutBox.Multiline = true;
+            this.OutPutBox.Name = "OutPutBox";
+            this.OutPutBox.ReadOnly = true;
+            this.OutPutBox.Size = new System.Drawing.Size(243, 61);
+            this.OutPutBox.TabIndex = 7;
+            // 
+            // Chars
+            // 
+            this.Chars.Location = new System.Drawing.Point(12, 3);
+            this.Chars.Multiline = true;
+            this.Chars.Name = "Chars";
+            this.Chars.ReadOnly = true;
+            this.Chars.Size = new System.Drawing.Size(515, 76);
+            this.Chars.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(93, 89);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 16);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "F*cking!!";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(539, 301);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Chars);
+            this.Controls.Add(this.OutPutBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InPutBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "BF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox InPutBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox OutPutBox;
+        private System.Windows.Forms.TextBox Chars;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Brain fuck/Form1.cs
+++ Brain fuck/Form1.cs
@@ -12,9 +12,90 @@ namespace Brain_fuck
 {
     public partial class Form1 : Form
     {
+        private byte[] BfText = new byte[1000];
+        private bool fkon;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void CurrentState()
+        {
+            Chars.Text = "";
+            for (int i = 0; i < 100; i++)
+            {
+                if(BfText[i] >= 10) Chars.Text += (char)BfText[i] + " ";
+                else Chars.Text += BfText[i] + " ";
+            }
+            Chars.Refresh();
+            Application.DoEvents();
+        }
+
+        private void Resetting()
+        {
+            for (int i = 0; i < 100; i++)
+                BfText[i] = 0;
+            OutPutBox.Text = "";
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Resetting();
+            CurrentState();
+            
+
+            int l = textBox1.Text.Length;
+            int ptr = 0;
+            int count = 0;
+            string[] Inputs = InPutBox.Text.Split(' ');
+
+
+            for(int i = 0;i < l; i++)
+            {
+                char ch = textBox1.Text[i];
+                switch (ch)
+                {
+                    case '+':
+                        BfText[ptr]++;
+                        break;
+                    case '-':
+                        BfText[ptr]--;
+                        break;
+                    case '>':
+                        ptr++;
+                        break;
+                    case '<':
+                        ptr--;
+                        break;
+                    case '.':
+                        OutPutBox.Text += (char)BfText[ptr];
+                        OutPutBox.Refresh();
+                        break;
+                    case ',':
+                        break;
+                    case '[':
+                        if (BfText[ptr] == 0)
+                            i = textBox1.Text.IndexOf(']', ptr);
+                        else
+                            count = i;
+                        break;
+                    case ']':
+                        if (BfText[ptr] != 0)
+                            i = count;
+                        break;
+                    default:
+                        break;
+                }
+                if(!fkon) CurrentState();
+            }
+            if(fkon) CurrentState();
+            OutPutBox.Text += "\r\n---Dice is great.---";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            fkon = checkBox1.Checked;
         }
     }
 }
